d4a540c17aa35a92eedec247b842de9bae00f259
socallmebertille <sarah.bejard@gmail.com>
2025-03-25 14:40:30 +0100

    final cpp01

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: saberton <saberton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:22:46 by saberton          #+#    #+#             */
-/*   Updated: 2025/03/25 14:38:32 by saberton         ###   ########.fr       */
+/*   Updated: 2025/03/25 14:40:02 by saberton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	main(int ac, char **av)
 {
-		Harl comment;
-
 	if (ac == 2)
-		comment.complain(av[1]);
+    {
+        std::string level(av[1]);
+		Harl comment;
+        comment.complain(level);
+    }
 	else
 		std::cout << "ERROR" << std::endl << "Progam should be called like : ./harlFilter <INSTRUCTION>" << std::endl;
 	return (0);
